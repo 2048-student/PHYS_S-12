86328b4d4bc74efa235b486e397efcbe0c0f73f6
Andrew Binder <abinder@invand.com>
2020-07-09 16:11:29 -0400

    Update index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>About Me</title>
+    <?php include("inc/htmlhead.php"); ?>
+</head>
+
+<body id="aboutme" style="width:100%">
+    <div class="divheader">
+        <?php include("header.php"); ?>
+    </div>
+    <div class="divmain">
+        <span>
+            <div class="divnav">
+                <?php include("nav.php"); ?>
+            </div>
+            <h2> About Me </h2>
+            <p> Hello! I am Andrew Binder, and this is my online curriculum vitae. </p>
+            <p>
+                The navigation bar will link to certain specifics about what I have accomplished thus far. You can contact me with the info below.
+            <p>
+            <p>
+                As a note, this website was coded entirely by me, with a little bit of help here and there with embellishment from my father and grandfather. None of the HTML, PHP, CSS, or JS here has been made with any 3rd-party software. I coded it to get a better understanding for web development while completing my CSCI E-12 course, and I have managed to learn a lot!
+            </p>
+            <p>Now, a quick note about myself!</p>
+            <p>
+                I was born in Bensonherst, Brooklyn on the 26th of July, 2003. I am a first generation Jewish/Ukrainian immigrant, and was born a few weeks after my parents moved from Odessa. My native language is Russian, but I quickly picked up English as a second native language as soon as I went to elementary school. I am currently native in Russian and English, and fluent in Spanish thanks to my schools' efforts. I went to PS24 from kindergarten to 4th grade, after which I went to the Speyer Legacy School from 6th grade to 8th grade, and now I have just finished up my junior year at Stuyvesant High School.
+            </p>
+            <p>
+                This brings us to the present and wraps up the TL;DR about myself! Feel free to explore the rest of my curriculum vitae as well as learn more in-depth info about me by using the navigation bar to the left!
+            </p>
+        </span>
+    </div>
+    <div class="divfooter">
+        <?php include("footer.php"); ?>
+    </div>
+</body>
+
+</html>
